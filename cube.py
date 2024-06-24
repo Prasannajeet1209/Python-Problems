9630247bee87b5603d7791fb6e561bcc9f156801
Prasannajeet Jain <99951062+Prasannajeet1209@users.noreply.github.com>
2024-06-24 23:22:41 +0530

    Add files via upload

---
+++ cube.py
@@ -0,0 +1,7 @@
+def cube(a):
+    c=a*a*a
+    return c
+a=int(input("Enter value 1 :"))
+
+c=cube(a)
+print("Cube is :",c)
---
+++ dic.py
@@ -0,0 +1,7 @@
+d={
+    "name":"Prasannajeet Jain",
+    "Age":20,
+    "Salary":54200,
+    "experience":10
+}
+print(d)
